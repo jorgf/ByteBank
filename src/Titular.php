a7e2d10a51aea0d2029fa0116ab51097a72ac133
jorgef <jorgeartur.f@gmail.com>
2023-11-29 14:12:17 -0300

    agora extende a classe pessoa

--- src/Titular.php
+++ src/Titular.php
@@ -1,37 +1,8 @@
 <?php
 
-class Titular {
-  private string $nomeTitular;
-  private Cpf $cpfTitular;
-  private Endereco $endereco;
-
-  public function __construct(Cpf $cpf, string $nome, Endereco $endereco) {
-    $this->cpfTitular = $cpf;
-    $this->validarNome($nome);
-    $this->nomeTitular = $nome;
-    $this->endereco = $endereco;
-  }
+class Titular extends Pessoa {
   
-  public function recuperarCpf() {
-    return $this->cpfTitular->recuperarCpf();
-  }
- 
-  public function definirNome(string $nome) {
-    $this->nomeTitular = $nome;
-  }
- 
-  public function recuperarNome() {
-    return $this->nomeTitular;
-  }
-
-  public function recuperarEndereco() {
-    return $this->endereco;
-  }
- 
-  private function validarNome(string $nome) {
-   if(mb_strlen($nome) < 5) {
-     echo "ERRO! o nome do titular precisa ter mais de 5 caracteres!\n";
-     exit();
-   }
+  public function __construct(Cpf $cpf, string $nome, Endereco $endereco) {
+    parent::__construct($nome, $cpf, $endereco);
   }
 }
