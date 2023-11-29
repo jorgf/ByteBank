f4ffb4218891ccab4d1607a4c64f68713c93f082
jorgef <jorgeartur.f@gmail.com>
2023-11-29 01:48:56 -0300

    classe cpf

---
+++ src/Cpf.php
@@ -0,0 +1,31 @@
+<?php
+
+class Cpf {
+  private $numero;
+  
+  public function __construct(string $numero) {
+    $this->validarNumero($numero);
+    $this->numero = $numero;
+
+  }
+
+  public function recuperarNumero() {
+    return $this->numero;
+  }
+
+  public function limparNumero($numero) {
+    $pattern = '/[^\d]/';
+    $replacement = "";
+    return preg_replace($pattern, $replacement, $numero);
+  }
+
+  public function validarNumero($numero) {
+    if (strlen($this->limparnumero($numero)) < 11) {
+      echo "CPF inválido!" . PHP_EOL;
+      exit();
+    }
+  }
+
+  //TODO - validar numero do CPF, ate agora foi feito apenas verificacao do tamanho 
+
+}
