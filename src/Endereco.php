7904eade795d9ad1d9f1e46a897f310bf91d3b95
jorgef <jorgeartur.f@gmail.com>
2023-11-29 12:17:15 -0300

    Criacao da classe endereco

---
+++ src/Endereco.php
@@ -0,0 +1,29 @@
+<?php
+
+class Endereco {
+  
+  private string $rua;
+  private string $bairro;
+  private string $numero;
+  private string $cidade;
+
+  public function __construct(string $rua, string $bairro, string $numero, string $cidade) {
+    $this->rua = $rua;
+    $this->bairro = $bairro;
+    $this->numero = $numero;
+    $this->cidade = $cidade;
+  }
+
+  public function recuperarRua(){
+    return $this->rua;
+  }
+  public function recuperarBairro(){
+    return $this->bairro;
+  }
+  public function recuperarNumero(){
+    return $this->numero;
+  }
+  public function recuperarCidade(){
+    return $this->cidade;
+  }
+}
