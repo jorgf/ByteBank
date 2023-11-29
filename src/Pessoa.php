1f730eaeee7bbe0fd851bbd654d7ae3597e77eca
jorgef <jorgeartur.f@gmail.com>
2023-11-29 14:11:05 -0300

    classe para abstracao de pessoas

---
+++ src/Pessoa.php
@@ -0,0 +1,39 @@
+<?php
+
+class Pessoa {
+  protected string $nome;
+  protected Cpf $cpf;
+  protected Endereco $endereco;
+
+  public function __construct(string $nome, Cpf $cpf, Endereco $endereco) {
+
+    $this->validarNome($nome);
+    $this->nome = $nome;
+    $this->cpf = $cpf;
+    $this->endereco = $endereco;
+  }
+
+  public function recuperarCpf() {
+    return $this->cpf->recuperarCpf();
+  }
+   
+  public function definirNome(string $nome) {
+    $this->nome = $nome;
+  }
+   
+  public function recuperarNome() {
+    return $this->nome;
+  }
+
+  public function recuperarEndereco() {
+    return $this->endereco;
+  }
+  
+  protected function validarNome(string $nome) {
+    if(mb_strlen($nome) < 5) {
+      echo "ERRO! o nome do titular precisa ter mais de 5 caracteres!\n";
+      exit();
+    }
+  }
+    
+}
